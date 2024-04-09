c9cd32583852849c6312a1a46393b20c79079e18
rimrim990 <lim990718@naver.com>
2024-04-09 15:46:45 +0900

    feat: 쿼리 파싱
    Co-authored-by: bronte000 <bronte000@naver.com>

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,5 +1,6 @@
 package webserver;
 
+import db.DataBase;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -8,16 +9,23 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.URISyntaxException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import utils.FileIoUtils;
 
 public class RequestHandler implements Runnable {
+
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+    public static final Pattern QUERY_PATTERN = Pattern.compile("^[^?#]+\\?([^#]+)");
 
     private Socket connection;
 
@@ -33,28 +41,59 @@ public class RequestHandler implements Runnable {
             InputStreamReader reader = new InputStreamReader(in);
             BufferedReader bufferedReader = new BufferedReader(reader);
 
-            String fileName = parseCommandPath(bufferedReader.readLine());
-            Map<String, String> headerDict = parseHeader(bufferedReader);
-            String filePath = makeFilePath(fileName, headerDict);
-            System.out.println(fileName + ", " + filePath);
-            byte[] body = FileIoUtils.loadFileFromClasspath(filePath);
+            String command = parseCommandPath(bufferedReader.readLine());
+            byte[] body = {};
+            String contentType = null;
+
+            if (isQuery(command)) {
+                ExecuteCommand(command);
+            }
+            if (isResource(command)) {
+                String filePath = makeFilePath(command);
+                body = FileIoUtils.loadFileFromClasspath(filePath);
+                contentType = Files.probeContentType(Path.of(filePath));
+            }
 
             DataOutputStream dos = new DataOutputStream(out);
-            response200Header(dos, body.length, Files.probeContentType(Path.of(filePath)));
+            response200Header(dos, body.length, contentType);
             responseBody(dos, body);
         } catch (IOException | URISyntaxException e) {
             logger.error(e.getMessage());
         }
     }
 
-    private String makeFilePath(String fileName, Map<String, String> headerDict) {
-        if (isStatic(fileName, headerDict)) {
+    private void ExecuteCommand(String command) {
+        String[] tokens = command.split("\\?");
+        String path = tokens[0];
+        Map<String, String> parameters = Arrays.stream(tokens[1].split("&"))
+            .map(url -> URLDecoder.decode(url, StandardCharsets.UTF_8))
+            .map(token -> token.split("="))
+            .collect(Collectors.toMap(token -> token[0], token -> token[1]));
+
+        if (path.startsWith("/user")) {
+            DataBase.addUser(new User(parameters.get("userId"), parameters.get("password"),
+                parameters.get("name"), parameters.get("email")));
+
+            System.out.println(DataBase.findUserById("test"));
+        }
+    }
+
+    private boolean isResource(String command) {
+        return !isQuery(command);
+    }
+
+    private boolean isQuery(String command) {
+        return QUERY_PATTERN.matcher(command).find();
+    }
+
+    private String makeFilePath(String fileName) {
+        if (isStatic(fileName)) {
             return "./static" + fileName;
         }
         return "./templates" + fileName;
     }
 
-    private boolean isStatic(String commandPath, Map<String, String> headerDict) {
+    private boolean isStatic(String commandPath) {
         return !commandPath.endsWith(".html") && !commandPath.endsWith(".ico");
     }
 
@@ -63,17 +102,17 @@ public class RequestHandler implements Runnable {
         return tokens[1];
     }
 
-    private Map<String, String> parseHeader(BufferedReader bufferedReader) throws IOException{
-        Map<String, String> headerDict = new HashMap<>();
-        String line = bufferedReader.readLine();
-        while (!"".equals(line)) {
-            if (line == null) return headerDict;
-            String [] tokens = line.split(": ");
-            headerDict.put(tokens[0].toLowerCase(), tokens[1]);
-            line = bufferedReader.readLine();
-        }
-        return headerDict;
-    }
+//    private Map<String, String> parseHeader(BufferedReader bufferedReader) throws IOException{
+//        Map<String, String> headerDict = new HashMap<>();
+//        String line = bufferedReader.readLine();
+//        while (!"".equals(line)) {
+//            if (line == null) return headerDict;
+//            String [] tokens = line.split(": ");
+//            headerDict.put(tokens[0].toLowerCase(), tokens[1]);
+//            line = bufferedReader.readLine();
+//        }
+//        return headerDict;
+//    }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String contentType) {
         try {
