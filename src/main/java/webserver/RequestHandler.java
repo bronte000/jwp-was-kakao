73437b05bc56a60bd821c362093d791a757c9ae0
rimrim990 <lim990718@naver.com>
2024-04-09 11:24:29 +0900

    feat: 정적 파일 서빙
    Co-authored-by: bronte000 <bronte000@naver.com>

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -7,8 +7,11 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
-
 import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import utils.FileIoUtils;
@@ -30,15 +33,29 @@ public class RequestHandler implements Runnable {
             InputStreamReader reader = new InputStreamReader(in);
             BufferedReader bufferedReader = new BufferedReader(reader);
 
-            String filePath = "./templates" + parseCommandPath(bufferedReader.readLine());
+            String fileName = parseCommandPath(bufferedReader.readLine());
+            Map<String, String> headerDict = parseHeader(bufferedReader);
+            String filePath = makeFilePath(fileName, headerDict);
+            System.out.println(fileName + ", " + filePath);
             byte[] body = FileIoUtils.loadFileFromClasspath(filePath);
 
             DataOutputStream dos = new DataOutputStream(out);
-            response200Header(dos, body.length);
+            response200Header(dos, body.length, Files.probeContentType(Path.of(filePath)));
             responseBody(dos, body);
         } catch (IOException | URISyntaxException e) {
             logger.error(e.getMessage());
         }
+    }
+
+    private String makeFilePath(String fileName, Map<String, String> headerDict) {
+        if (isStatic(fileName, headerDict)) {
+            return "./static" + fileName;
+        }
+        return "./templates" + fileName;
+    }
+
+    private boolean isStatic(String commandPath, Map<String, String> headerDict) {
+        return !commandPath.endsWith(".html") && !commandPath.endsWith(".ico");
     }
 
     private String parseCommandPath(String commandLine) {
@@ -46,10 +63,22 @@ public class RequestHandler implements Runnable {
         return tokens[1];
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private Map<String, String> parseHeader(BufferedReader bufferedReader) throws IOException{
+        Map<String, String> headerDict = new HashMap<>();
+        String line = bufferedReader.readLine();
+        while (!"".equals(line)) {
+            if (line == null) return headerDict;
+            String [] tokens = line.split(": ");
+            headerDict.put(tokens[0].toLowerCase(), tokens[1]);
+            line = bufferedReader.readLine();
+        }
+        return headerDict;
+    }
+
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String contentType) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Type:" + contentType + "\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
